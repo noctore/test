81297fa6cc3bd0b71f371e5a3040325be4d6cc56
noctore <ariquciha58@gmail.com>
2022-08-14 20:12:18 +0700

    fixed variable naming

--- max_min.py
+++ max_min.py
@@ -1,22 +1,27 @@
+"""
+Functions:
+- max(list)
+"""
+
 list_1 = [-99999, -9, 89,-80, 234, -10000, 99999999]
 
-def max(list):
+def max(my_list):
     a = list_1[0]
-    for x in list:
+    for x in my_list:
         if x >= a:
             a = x
         else:
             continue
-    print (f"nilai maksimum adalah: {a}")
+    print(f"nilai maksimum adalah: {a}")
 
-def min(list):
+def min(my_list):
     a = list_1[0]
-    for x in list:
+    for x in my_list:
         if x <= a:
             a = x
         else:
             continue
-    print (f"nilai minimum adalah: {a}")
+    print(f"nilai minimum adalah: {a}")
 
 max(list_1)
 
